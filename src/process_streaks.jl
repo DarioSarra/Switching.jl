122d80fe3938461db5730c0ed69d52a0d6a7f404
DarioSarra <dariosarra@gmail.com>
2020-05-04 10:59:09 +0100

    separated poking time from roi time

--- src/process_streaks.jl
+++ src/process_streaks.jl
@@ -17,33 +17,26 @@ function process_streaks(df::DataFrames.AbstractDataFrame)
         Prev_Reward = findlast(dd[!,:Reward]).== nothing ? 0 : findprev(dd[!,:Reward], findlast(dd[!,:Reward])-1),
         Poking_duration = (dd[end,:PokeOut]-dd[1,:PokeIn]),
         Trial_duration = (dd[end,:ROI_Out]-dd[1,:ROI_In]),
-        Start = (dd[1,:PokeIn]),
-        Stop = (dd[end,:PokeOut]),
+        Start_poking = (dd[1,:PokeIn]),
+        Stop_poking = (dd[end,:PokeOut]),
+        Start_trial = (dd[1,:ROI_In]),
+        Stop_trial = (dd[end,:ROI_Out]),
         Pre_Interpoke = nrow(dd) > 1 ? maximum(skipmissing(dd[!,:Pre_Interpoke])) : missing,
         Post_Interpoke = nrow(dd) > 1 ? maximum(skipmissing(dd[!,:Post_Interpoke])) : missing,
         PokeSequence = [SVector{nrow(dd),Union{Bool,Missing}}(dd[!,:Reward])],
-        #Stim = dd[1,:Stim],
-        #StimFreq = dd[1,:StimFreq],
-        #Wall = dd[1,:Wall],
         Protocol = dd[1,:Protocol],
-        #Correct_start = dd[1,:Correct],
-        #Correct_leave = !dd[end,:Correct],
-        #Block = dd[1,:Block],
-        #Streak_within_Block = dd[1,:Streak_within_Block],
         Side = dd[1,:Side],
         ReverseTrial = dd[1,:ReverseTrial]
         )
-        # for s in dayly_vars_list
-        #     if s in names(df)
-        #         dt[!,s] .= df[1, s]
-        #     end
-        # end
         return dt
     end
     streak_table[!,:Prev_Reward] = [x .== nothing ? 0 : x for x in streak_table[:,:Prev_Reward]]
     streak_table[!,:AfterLast] = streak_table[!,:Num_pokes] .- streak_table[!,:Last_Reward];
     streak_table[!,:BeforeLast] = streak_table[!,:Last_Reward] .- streak_table[!,:Prev_Reward].-1;
-    prov = lead(streak_table[!,:Start],default = 0.0) .- streak_table[!,:Stop];
-    streak_table[!,:Travel_to] = [x.< 0 ? 0 : x for x in prov]
+    prov = lead(streak_table[!,:Start_poking],default = 0.0) .- streak_table[!,:Stop_poking];
+    streak_table[!,:Poking_Travel_to] = [x.< 0 ? 0 : x for x in prov]
+    prov = lead(streak_table[!,:Start_trial],default = 0.0) .- streak_table[!,:Stop_trial];
+    streak_table[!,:Trial_Travel_to] = [x.< 0 ? 0 : x for x in prov]
+    streak_table[!,:Travel_to] = streak_table.Poking_Travel_to + streak_table.Trial_Travel_to
     return streak_table
 end
